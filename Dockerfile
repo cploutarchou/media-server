77959de19d71f4a800a11e929b1e4860d02917b7
Christos Ploutarchou <cploutarchou@gmail.com>
2024-09-13 19:52:04 +0000

    allow subfolders

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
 FROM python:3.12
 
-WORKDIR web
+WORKDIR /web
 
+# Copy the requirements file and install Python dependencies
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
+
+# Install Gunicorn separately if not in requirements.txt
 RUN pip install gunicorn
 
+# Copy your web application to the Docker container
 COPY web ./web
 
+# Command to run your application using Gunicorn
 CMD ["gunicorn", "-b", "0.0.0.0:5000", "web:app"]
--- web/__init__.py
+++ web/__init__.py
@@ -10,33 +10,37 @@ app = Flask(__name__)
 UPLOAD_FOLDER = '/home/media'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+
 # Logging Configuration
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 handler = RotatingFileHandler('web.log', maxBytes=10000, backupCount=3)
 logger.addHandler(handler)
 
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
     try:
-        if 'file' not in request.files:
-            raise ValueError("No file part")
+        # Change 'file' to 'myFile' to match the MultipartEncoder field
+        if 'myFile' not in request.files:
+            raise ValueError("No myFile part")
 
-        file = request.files['file']
+        file = request.files['myFile']
         if file.filename == '':
             raise ValueError("No selected file")
 
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return jsonify(success=True), 200
+
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        return jsonify(success=True, filename=filename), 200
+
+
     except Exception as e:
         logger.error(f"Error during upload: {str(e)}")
         return jsonify(error=str(e)), 500
 
-
-@app.route('/<filename>', methods=['GET'])
+@app.route('/<path:filename>', methods=['GET'])
 def get_image(filename):
     try:
         return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
@@ -44,3 +48,6 @@ def get_image(filename):
         logger.error(f"File not found: {filename}")
         return jsonify(error="File not found"), 404
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
